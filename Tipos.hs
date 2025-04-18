852d58f796f8b4ab0940640fe48adc75475ce553
Davi Paiva Sendin <71078956+DavPaiSen@users.noreply.github.com>
2025-04-18 16:16:59 -0300

    Add files via upload

--- Tipos.hs
+++ Tipos.hs
@@ -3,7 +3,6 @@ module Tipos (
         Status(..),
         Prioridade(..),
         Categoria(..),
-        listaTarefas,
         Day
 )where
 
@@ -17,55 +16,6 @@ data Tarefa = Tarefa
      , status :: Status
      , prioridade :: Prioridade
      , categoria :: Categoria
-     , prazo :: Maybe Day -- Usando Data.Time.Calendar
+     , prazo :: Maybe Day 
      , tags :: [String]
      } deriving (Show, Eq)
-
-listaTarefas :: [Tarefa]
-listaTarefas =
-    [ Tarefa
-        { idTarefa = 1
-        , descricao = "Estudar Haskell"
-        , status = Pendente
-        , prioridade = Alta
-        , categoria = Estudos
-        , prazo = Just (fromGregorian 2023 12 15)
-        , tags = ["programação", "importante"]
-        }
-    , Tarefa
-        { idTarefa = 2
-        , descricao = "Fazer relatório mensal"
-        , status = Concluida
-        , prioridade = Media
-        , categoria = Trabalho
-        , prazo = Just (fromGregorian 2023 11 30)
-        , tags = ["relatório", "chefe"]
-        }
-    , Tarefa
-        { idTarefa = 3
-        , descricao = "Comprar presentes de Natal"
-        , status = Pendente
-        , prioridade = Baixa
-        , categoria = Pessoal
-        , prazo = Just (fromGregorian 2023 12 24)
-        , tags = ["compras", "familia"]
-        }
-    , Tarefa
-        { idTarefa = 4
-        , descricao = "Reunião com equipe"
-        , status = Pendente
-        , prioridade = Alta
-        , categoria = Trabalho
-        , prazo = Nothing  -- Sem prazo definido
-        , tags = ["reunião"]
-        }
-    , Tarefa
-        { idTarefa = 5
-        , descricao = "Ler livro sobre algoritmos"
-        , status = Pendente
-        , prioridade = Media
-        , categoria = Estudos
-        , prazo = Just (fromGregorian 2024 01 31)
-        , tags = ["leitura", "aprendizado"]
-        }
-    ]
