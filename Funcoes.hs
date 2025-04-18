fe2ed5d57578da2137c1141dc60d9832fb5f8c5e
Davi Paiva Sendin <71078956+DavPaiSen@users.noreply.github.com>
2025-04-18 17:24:04 -0300

    Add files via upload

--- Funcoes.hs
+++ Funcoes.hs
@@ -35,30 +35,24 @@ marcarConcluida n (x:xs)
             Right l -> Right (x : l)
 
 --funcoes avancadas
+
+recursaoGenerica :: (Tarefa -> Bool) -> [Tarefa] -> [Tarefa]
+recursaoGenerica _ [] = []
+recursaoGenerica condicao (x:xs)
+    |condicao x = x : recursaoGenerica condicao xs
+    |otherwise = recursaoGenerica condicao xs
+
 listarPorCategoria :: Categoria -> [Tarefa] -> [Tarefa]
-listarPorCategoria _ [] = []
-listarPorCategoria c (x:xs)
-    |categoria x == c = x : listarPorCategoria c xs
-    |otherwise = listarPorCategoria c xs
+listarPorCategoria c l = recursaoGenerica (\t -> categoria t == c) l
 
 listarPorPrioridade :: Prioridade -> [Tarefa] -> [Tarefa]
-listarPorPrioridade _ [] = []
-listarPorPrioridade p (x:xs)
-    |prioridade x == p = x : listarPorPrioridade p xs
-    |otherwise = listarPorPrioridade p xs
+listarPorPrioridade p l = recursaoGenerica (\t -> prioridade t == p) l
 
 ordenarPorPrioridade :: [Tarefa] -> [Tarefa]
 ordenarPorPrioridade l = concat [listarPorPrioridade Alta l, listarPorPrioridade Media l, listarPorPrioridade Baixa l]
 
 filtrarPorStatus :: Status -> [Tarefa] -> [Tarefa]
-filtrarPorStatus _ [] = []
-filtrarPorStatus s (x:xs)
-    |status x == s = x : filtrarPorStatus s xs
-    |otherwise = filtrarPorStatus s xs
+filtrarPorStatus s l = recursaoGenerica (\t -> status t == s) l
 
 buscarPorPalavraChave :: String -> [Tarefa] -> [Tarefa]
-buscarPorPalavraChave _ [] = []
-buscarPorPalavraChave p (x:xs)
-    |elem p (tags x) = x : buscarPorPalavraChave p xs
-    |otherwise = buscarPorPalavraChave p xs
-
+buscarPorPalavraChave p l = recursaoGenerica (\t -> elem p (tags t)) l
