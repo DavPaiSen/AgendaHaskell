b94fcc7fe87bbf6067ebe82fb88fafde74323b0b
Gabriel Henrique <104363967+gabrielhca@users.noreply.github.com>
2025-04-22 15:56:55 -0300

    import Tipos

--- Persistencia.hs
+++ Persistencia.hs
@@ -1,4 +1,5 @@
 module Persistencia(salvarEmArquivo, carregarDeArquivo) where
+import Tipos
 salvarEmArquivo :: FilePath -> [Tarefa] -> IO ()
 salvarEmArquivo caminho tarefas = writeFile caminho (show tarefas)
 
